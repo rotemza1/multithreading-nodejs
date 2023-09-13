921bdfc67ac2c0a8145aef21e04218e9c8f6904d
rotem.zaig <rotem.zaig@zooz.com>
2023-09-13 14:29:17 +0300

    added fibonacci example

---
+++ server-stresser.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const rpm = 500;
+const delay = (60 * 1000)/ rpm;
+let requestCounter = rpm;
+let requestDoneCounter = rpm;
+let errors = 0;
+
+const startTime = Date.now();
+
+const getFibonacciResult = async (counter) => {
+    //query the server at a rate of 500 rpm to calculate the Fibonacci number for 37
+    const res = await axios.get('http://localhost:3000?n=37&useThreads=true').catch(onError);
+    console.log('requests done:', rpm - requestDoneCounter);
+    requestDoneCounter--;
+    console.log(counter, ' || ', res.data);
+    requestDoneCounter === 1 && console.log(`took ${Date.now() - startTime} ms with ${errors} errors` );
+}
+
+const onError = () => {
+    errors++;
+    return {data: 'error'}
+}
+
+const fire = () => {
+    if (!requestCounter) {
+        return;
+    }
+    getFibonacciResult(requestCounter);
+    requestCounter--;
+    setTimeout(fire, delay);
+}
+
+fire();
